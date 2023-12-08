3ca3a405217038ec48efe52a7d36f95d712fcceb
shshnn <ssss5489@naver.com>
2023-12-08 23:19:57 +0900

    end todo  function

--- src/todo/index.js
+++ src/todo/index.js
@@ -146,7 +146,7 @@ return (
                                 <Checkbox id={todos.id} onChange={(e)=>{
                                     endChangeTodo(e.target.id)}} className="todo-ck" checked={false}>
                         
-                        {todos.todo_contents}
+                        {todos.todo_contents} 
                         </Checkbox>   
                     )
                 }
